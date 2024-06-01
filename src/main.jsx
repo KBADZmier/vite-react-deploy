0440bada1840d2fb4e4b22ef2a8df3d46d453886
KBADZmier <Bidzkok@gmail.com>
2024-06-02 01:01:11 +0200

    Apka

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,6 @@ import { Provider } from 'react-redux';
 import App from './App';
 import store from '/store';
 import { configureStore } from '@reduxjs/toolkit';
-
-
-
-
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
     <React.StrictMode>
