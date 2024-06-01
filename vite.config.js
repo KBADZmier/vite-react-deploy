99e0ae84226f4b0759b344d7fd15b1713889bcef
KBADZmier <Bidzkok@gmail.com>
2024-06-02 01:11:55 +0200

    Ez

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/vite-react-deploy/', // Upewnij się, że jest to ścieżka do twojego repozytorium
+  base: "/vite-react-deploy/", // Upewnij się, że jest to ścieżka do twojego repozytorium
 })
